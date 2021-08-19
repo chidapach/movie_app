3f031bb83c96f87423f0ce541cdb98358b9f98ab
chidapach <chidapa.ch@chococrm.com>
2021-08-19 13:40:43 +0700

    [Add] movie_model

---
+++ lib/model/movie.dart
@@ -0,0 +1,19 @@
+class Movie{
+  final int id;
+  final double popularity;
+  final String title;
+  final String backPoster;
+  final String poster;
+  final String overview;
+  final double rating;
+
+  Movie(
+    this.id, 
+    this.popularity, 
+    this.title, 
+    this.backPoster, 
+    this.poster, 
+    this.overview, 
+    this.rating   
+  );
+}
